93bad61bbc0fe3e76da482834447f235d90c63af
Colorblindy <64820222+Colorblindy@users.noreply.github.com>
2025-03-08 11:58:01 +0700

    Nashatra fix for LSB

--- Nashatra.lua
+++ Nashatra.lua
@@ -8,9 +8,6 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local system = loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/Colorblindy/Modules/refs/heads/main/Modular.lua"))();
 local AnimationTrack = loadstring(game:GetService("HttpService"):GetAsync("https://github.com/MechaXYZ/modules/raw/main/Anitracker.lua"))()
 
-shared.system = system
-shared.AnimationTrack = AnimationTrack
-
 local assets = nil
 local anims = loadstring
 
@@ -23,6 +20,23 @@ local root = humanoid.RootPart or character:FindFirstChild("HumanoidRootPart")
 
 local remote = Instance.new("RemoteEvent")
 remote.Name = "Hotkey"
+remote.Parent = character
+NLS([[
+    local UserInputService = game:GetService("UserInputService")
+    local remote = ...
+
+    UserInputService.InputBegan:Connect(function(input, gameProcessedEvent)
+        if gameProcessedEvent then return end
+
+        if input.KeyCode == Enum.KeyCode.E then
+            remote:FireServer("E")
+        elseif input.KeyCode == Enum.KeyCode.R then
+            remote:FireServer("R")
+        elseif input.KeyCode == Enum.KeyCode.F then
+            remote:FireServer("F")
+        end
+    end)
+]], player.PlayerGui, remote)
 
 --# model setups
 
@@ -67,7 +81,7 @@ local client = Instance.new("RemoteEvent")
 client.Name = "Client"
 client.Parent = character
 NLS([[
-    local remote = ...
+    local client = ...
 
     client.OnClientEvent:Connect(function(case, special)
         if not special or typeof(special) ~= "table" then
@@ -171,6 +185,7 @@ function StudBlood(part : BasePart, count : number)
         end
     end)
 end
+
 
 -- Make Hitbox
 function BunkerHillHitbox(offset : CFrame | nil, size : Vector3, combo)
@@ -316,4 +331,97 @@ bunkerHill.Activated:Connect(function()
     task.delay(secondDelay, function()
         bunkerHillDebounces.deb = false
     end)
+end)
+
+--# hotkey
+
+remote.OnServerEvent:Connect(function(plr, key)
+    print(using, key)
+    if using == "None" then
+        return
+    end
+
+    if key == "E" then
+        if using == "BunkerHill" then
+            if bunkerHillDebounces.E then return end
+            bunkerHillDebounces.E = true
+
+            local velo:BodyVelocity = system:Velocity(character, Vector3.new(0, 8, 0), true, 6, nil, true, nil)
+            system.MakeSound(GetSound(root, "JetBoots"), root, {Volume = 1})
+            local exhaust = task.spawn(function()
+                while true do
+                    local part = Instance.new("Part")
+                    part.Anchored = false
+                    part.CanCollide = true
+                    part.CanQuery = false
+                    part.Size = Vector3.new(1, 1, 1)
+                    part.Color = Color3.new(1, 0.5, 0)
+                    part.Shape = Enum.PartType.Cylinder
+                    part.Material = "Plastic"
+
+                    part.TopSurface = Enum.SurfaceType.Studs
+                    part.BottomSurface = Enum.SurfaceType.Studs
+
+                    part.CFrame = root.CFrame * CFrame.new(0, -3.1, 0)
+                    part.Parent = workspace
+
+                    part.AssemblyLinearVelocity = Vector3.new(
+                        math.random(-7, 7), 0, math.random(-7, 7)
+                    ) * 1.5
+                    part.AssemblyAngularVelocity = Vector3.new(
+                        math.random(-10, 10), math.random(-10, 10), math.random(-10, 10)
+                    )
+
+                    NS([[
+                        local StudBlood, system = ...
+                        local part = script.Parent
+                        
+                        part.Touched:Connect(function(hit)
+                            if hit.Parent == owner.Character or part:GetAttribute("RunOut") then
+                                return
+                            end
+
+                            if hit.Parent:IsA("Model") and hit.Parent:FindFirstChildOfClass("Humanoid") then
+                                part.CanTouch = false
+
+                                local fhum:Humanoid = hit.Parent:FindFirstChildOfClass("Humanoid")
+                                local fplr = game.Players:GetPlayerFromCharacter(hit.Parent)
+                                
+                                if fhum then
+                                    if fhum.Health > 0 then
+                                        StudBlood(fhum.RootPart, math.random(3, 10))
+                                    end
+
+                                    system:Damage(owner.Character, hit.Parent, owner, 10, {"MaxHP%"}) 
+                                end
+                            end
+                        end)
+                    ]], part, StudBlood, system)
+
+                    task.delay(3, function()
+                        part:SetAttribute("RunOut", true)
+                        part.Color = Color3.new(0, 0, 0)
+                    end)
+                    Debris:AddItem(part, 3.5)
+
+                    task.wait(0.04)
+                end
+            end)
+
+            task.delay(6, function()
+                task.cancel(exhaust)
+            end)
+
+            task.wait(9)
+            bunkerHillDebounces.E = false
+        end
+    elseif key == "R" then
+        if using == "BunkerHill" then
+            
+        end
+    elseif key == "F" then
+        if using == "BunkerHill" then
+            
+        end
+    end
 end)
